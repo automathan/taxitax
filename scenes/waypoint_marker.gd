640b0de1175a4a6a72de23eaaf5c4ed29f4f8b19
Jonna@Hufsa <jona.jorg@gmail.com>
2025-10-04 17:05:16 +0200

    Waypoint markers

--- scenes/waypoint_marker.gd
+++ scenes/waypoint_marker.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var sprite = $Sprite3D
+@onready var area = $Area3D
 
 func _ready() -> void:
 	var tween = get_tree().create_tween()
@@ -8,3 +9,16 @@ func _ready() -> void:
 	tween.chain().tween_property(sprite, 'offset', Vector2(0, 4), .5)
 	tween.chain().tween_property(sprite, 'offset', Vector2(0, 0), .5)
 	tween.set_loops(0)
+
+	area.area_entered.connect(_area_entered)
+
+func _area_entered(other: Area3D):
+	var parent = other.get_parent()
+
+	if parent is Car:
+		car_arrived()
+
+func car_arrived():
+	var tween = get_tree().create_tween()
+	tween.chain().tween_property(sprite, 'modulate', Color.TRANSPARENT, 2)
+	tween.chain().tween_callback(queue_free)
