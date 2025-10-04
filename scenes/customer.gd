6f108d9dd81fe2fcd6c092dc8d60fd189737349e
Jonna@Hufsa <jona.jorg@gmail.com>
2025-10-04 22:02:06 +0200

    Bugfixes

--- scenes/customer.gd
+++ scenes/customer.gd
@@ -5,9 +5,10 @@ extends Node3D
 @onready var death_area = $DeathArea
 @onready var sprite = $Sprite3D
 @onready var sfx = $AudioStreamPlayer
-@onready var waypoints = $"../Waypoints"
-@onready var car = $"../Car"
 
+@export var waypoints: Node3D
+@export var car: Node3D
+@export var dialogue: Control
 @export var sprite_variants: Array[Texture2D]
 
 var pickup_template = 'Picking Up\n[%s%s%s%s%s]'
@@ -20,7 +21,6 @@ func _ready() -> void:
 	death_area.area_entered.connect(_death_area_entered)
 	sprite.texture = sprite_variants[randi() % len(sprite_variants)]
 	sprite.flip_h = (randi() % 2) == 0
-	waypoint = waypoints.get_random_waypoint()
 
 func _death_area_entered(other: Area3D):
 	var parent = other.get_parent()
@@ -35,23 +35,27 @@ func pick_up():
 		entering = true
 
 func _physics_process(delta: float) -> void:
-	if car.busy:
+	if not fading and car and car.busy:
 		entering = false
 		label.text = 'TAXI!'
 		enter_time = 0
 
 	if entering and not fading:
-		enter_time += delta * 2
+		enter_time += delta * 3
 		label.text = pickup_template % generate_marker_array()
 
 		if enter_time > 6:
-			label.text = ''
-			sfx.play()
-			fade_out()
-			entering = false
-			waypoint.activate()
-			$"../CanvasLayer/Footer/NinePatchRect".set_content('Take me to %s' % waypoint.display_name)
-			car.busy = true
+			enter_taxi()
+
+func enter_taxi():
+	waypoint = waypoints.get_random_waypoint()
+	label.text = ''
+	sfx.play()
+	fade_out()
+	entering = false
+	waypoint.activate()
+	dialogue.set_content('Take me to %s' % waypoint.display_name)
+	car.busy = true
 
 func fade_out():
 	fading = true
