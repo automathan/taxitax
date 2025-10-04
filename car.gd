17624d4afa6c7094bb37d6fcba21c50cb8088fe0
Jonna@Hufsa <jona.jorg@gmail.com>
2025-10-04 13:25:35 +0200

    New car sprite

--- car.gd
+++ car.gd
@@ -1,8 +1,13 @@
 extends VehicleBody3D
 
 var max_steer = 1
-var engine_power = 500
+var engine_power = 600
+@onready var sprite = $AnimatedSprite3D
+
+func _ready() -> void:
+	sprite.play()
 
 func _physics_process(delta: float) -> void:
-	steering = move_toward(steering, Input.get_axis('ui_right', 'ui_left') * max_steer, delta * 10)
+	steering = move_toward(steering, Input.get_axis('ui_right', 'ui_left') * max_steer, delta * 40)
 	engine_force = Input.get_axis('ui_down', 'ui_up') * engine_power
+	sprite.speed_scale = linear_velocity.length()
