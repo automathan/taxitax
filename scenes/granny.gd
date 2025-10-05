a0941de51e5a2f7ee0f496afbdc59c881764cf1c
Jonna@Hufsa <jona.jorg@gmail.com>
2025-10-05 11:47:46 +0200

    Grandma and other worldbuilding

---
+++ scenes/granny.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@onready var death_area = $DeathArea
+@onready var sprite = $Sprite3D
+
+@export var splat_scene: PackedScene
+
+func _ready() -> void:
+	death_area.area_entered.connect(_death_area_entered)
+
+	var tween = get_tree().create_tween()
+	tween.chain().tween_interval(.8)
+	tween.chain().tween_property(sprite, 'offset', Vector2(0, 1), .2)
+	tween.chain().tween_property(sprite, 'offset', Vector2(0, 0), .2)
+	tween.set_loops(0)
+
+func _death_area_entered(other: Area3D):
+	var parent = other.get_parent()
+
+	if parent is Car:
+		die()
+
+func die():
+	print('KILLED')
+	Economy.add_cash(-100)
+	var splat = splat_scene.instantiate()
+	get_parent().add_child(splat)
+	splat.global_position = global_position
+	queue_free()
