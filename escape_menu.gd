91313c255863c4c8ab1ce7f1385f7905b098571a
Jonna@Hufsa <jona.jorg@gmail.com>
2025-10-05 19:28:56 +0200

    Fix escape menu

--- escape_menu.gd
+++ escape_menu.gd
@@ -8,8 +8,25 @@ extends Control
 @onready var footer = $"../Footer"
 @onready var crimes = $"../Crimes"
 
+func _ready() -> void:
+	$NinePatchRect/Button.pressed.connect(toggle_escape)
+	$NinePatchRect/Button3.pressed.connect(func(): get_tree().quit())
+	$NinePatchRect/Button2.pressed.connect(toggle_fullscreen)
+
+func toggle_fullscreen():
+	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		$NinePatchRect/Button2.text = 'FULLSCREEN'
+	elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		$NinePatchRect/Button2.text = 'WINDOWED'
+
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed('ui_cancel') and not Economy.is_game_over:
+	if event.is_action_pressed('ui_cancel'):
+		toggle_escape()
+
+func toggle_escape():
+	if not Economy.is_game_over:
 		get_tree().paused = not get_tree().paused
 		sfx.pitch_scale = 1.2 if get_tree().paused else .8
 		sfx.play()
